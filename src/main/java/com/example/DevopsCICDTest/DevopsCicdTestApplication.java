47c5883402c7cd8682cba282fc5b7a6a25e9b89c
vivekananth <vivek82.aws@gmail.com>
2020-06-14 12:58:31 -0400

    Test

---
+++ src/main/java/com/example/DevopsCICDTest/DevopsCicdTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.DevopsCICDTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsCicdTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsCicdTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DevopsCICDTest/DevopsCicdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DevopsCICDTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsCicdTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
